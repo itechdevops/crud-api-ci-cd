548ea73d1c1de4a245a29ed5e601e3bf6cf7520d
Renato Damazio <renatodamazio@outlook.com.br>
2026-03-01 00:18:43 -0300

    chore: initial spring boot project

---
+++ src/main/java/br/com/renatodamazio/crud_api_ci_cd/CrudApiCiCdApplication.java
@@ -0,0 +1,13 @@
+package br.com.renatodamazio.crud_api_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/renatodamazio/crud_api_ci_cd/CrudApiCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renatodamazio.crud_api_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
